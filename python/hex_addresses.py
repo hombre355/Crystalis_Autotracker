d90d5adaaa482c65914716a2d4b14c27182fa7fa
Michael Rowe <roweboat87@gmail.com>
2023-09-23 20:52:36 -0500

    fleshed out hex address list some more

--- python/hex_addresses.py
+++ python/hex_addresses.py
@@ -3,24 +3,47 @@ sword_list = ["Wind_Sword",       0x6430, 0x00, False], \
              ["Fire_Sword",       0x6431, 0x01, False], \
              ["Water_Sword",      0x6432, 0x02, False], \
              ["Thunder_Sword",    0x6433, 0x03, False], \
+             ["Crystalis_Sword",  0x6430, 0x04, False], \
              ["Wind_Ball",        0x643C, 0x05, False], \
-             ["Fire_Ball",        0x643D, 0x07, False], \
-             ["Water_Ball",       0x643E, 0x09, False], \
-             ["Thunder_Ball",     0x643F, 0x0B, False], \
              ["Wind_Bracelet",    0x643C, 0x06, False], \
+             ["Fire_Ball",        0x643D, 0x07, False], \
              ["Fire_Bracelet",    0x643D, 0x08, False], \
+             ["Water_Ball",       0x643E, 0x09, False], \
              ["Water_Bracelet",   0x643E, 0x0A, False], \
+             ["Thunder_Ball",     0x643F, 0x0B, False], \
              ["Thunder_Bracelet", 0x643F, 0x0C, False]
 
 # [name of item, hex location start, hex location end, hex number if obtained, obtained or not
-magic_list = ["Refresh",   0x6458, 0x645F, 0x41, False], \
-             ["Paralysis", 0x6458, 0x645F, 0x42, False], \
-             ["Telepathy", 0x6458, 0x645F, 0x43, False], \
-             ["Teleport",  0x6458, 0x645F, 0x44, False], \
-             ["Recover",   0x6458, 0x645F, 0x45, False], \
-             ["Barrier",   0x6458, 0x645F, 0x46, False], \
-             ["Change",    0x6458, 0x645F, 0x47, False], \
-             ["Flight",    0x6458, 0x645F, 0x48, False]
+shield_list = ["Carapace_Shield", 0x6438, 0x643B, 0x0D, False], \
+              ["Bronze_Shield",   0x6438, 0x643B, 0x0E, False], \
+              ["Platinum_Shield", 0x6438, 0x643B, 0x0F, False], \
+              ["Mirror_Shield",   0x6438, 0x643B, 0x10, False], \
+              ["Ceramic_Shield",  0x6438, 0x643B, 0x11, False], \
+              ["Sacred_Shield",   0x6438, 0x643B, 0x12, False], \
+              ["Battle_Shield",   0x6438, 0x643B, 0x13, False], \
+              ["Psycho_Shield",   0x6438, 0x643B, 0x14, False]
+
+# [name of item, hex location start, hex location end, hex number if obtained, obtained or not
+armor_list = ["Tanned_Hide",    0x6434, 0x6437, 0x15, False], \
+             ["Leather_Armor",  0x6434, 0x6437, 0x16, False], \
+             ["Bronze_Armor",   0x6434, 0x6437, 0x17, False], \
+             ["Platinum_Armor", 0x6434, 0x6437, 0x18, False], \
+             ["Soldier_Suit",   0x6434, 0x6437, 0x19, False], \
+             ["Ceramic_Suit",   0x6434, 0x6437, 0x1A, False], \
+             ["Battle_Armor",   0x6434, 0x6437, 0x1B, False], \
+             ["Psycho_Armor",   0x6434, 0x6437, 0x1C, False]
+
+# [name of item, hex location start, hex location end, hex number if obtained, obtained or not
+consumable_list = ["Medical_Herb",   0x6440, 0x6447, 0x1D, False], \
+                  ["Antidote",       0x6440, 0x6447, 0x1E, False], \
+                  ["Lysis_Plant",    0x6448, 0x644F, 0x1F, False], \
+                  ["Fruit_of_Lime",  0x6440, 0x6447, 0x20, False], \
+                  ["Fruit_of_Power", 0x6440, 0x6447, 0x21, False], \
+                  ["Magic_Ring",     0x6440, 0x6447, 0x22, False], \
+                  ["Fruit_of_Repun", 0x6440, 0x6447, 0x23, False], \
+                  ["Warp_Boots",     0x6440, 0x6447, 0x24, False], \
+                  ["Opel_Statue",    0x6440, 0x6447, 0x26, False], \
+                  ["Alarm_Flute",    0x6440, 0x6447, 0x31, False]
 
 # [name of item, hex location start, hex location end, hex number if obtained, obtained or not
 worn_list = ["Gas_Mask",      0x6448, 0x644F, 0x29, False], \
@@ -33,32 +56,31 @@ worn_list = ["Gas_Mask",      0x6448, 0x644F, 0x29, False], \
             ["Shield_Ring",   0x6448, 0x644F, 0x30, False]
 
 # [name of item, hex location start, hex location end, hex number if obtained, obtained or not
-Event_list_1 = ["Fruit_of_Repun", 0x6448, 0x644F, 0x23, False], \
-               ["Onyx_Statue",    0x6450, 0x6457, 0x25, False], \
-               ["Opel_Statue",    0x6448, 0x644F, 0x26, False], \
-               ["Insect_Flute",   0x6448, 0x644F, 0x27, False], \
-               ["Lime_Flute",     0x6448, 0x644F, 0x28, False], \
-               ["Alarm_Flute",    0x6448, 0x644F, 0x31, False], \
+Event_list_1 = ["Onyx_Statue",    0x6450, 0x6457, 0x25, False], \
+               ["Insect_Flute",   0x6450, 0x6457, 0x27, False], \
+               ["Flute_of_Lime",  0x6450, 0x6457, 0x28, False], \
+               ["Windmill_Key",   0x6450, 0x6457, 0x32, False], \
                ["Prison_Key",     0x6450, 0x6457, 0x33, False], \
-               ["Styx_Key",       0x6448, 0x644F, 0x34, False], \
-               ["Fog_Lamp",       0x6448, 0x644F, 0x35, False], \
-               ["Shell_Flute",    0x6448, 0x644F, 0x36, False], \
-               ["Xray_Glasses",   0x6448, 0x644F, 0x37, False], \
-               ["Broken_Statue",  0x6448, 0x644F, 0x38, False], \
-               ["Glow_Lamp",      0x6448, 0x644F, 0x39, False], \
-               ["Gold_Statue",    0x6448, 0x644F, 0x3A, False], \
-               ["Love_Pendant",   0x6448, 0x644F, 0X3B, False], \
-               ["Karissa_Plant",  0x6448, 0x644F, 0x3C, False], \
-               ["Ivory_Statue",   0x6448, 0x644F, 0x3D, False]
+               ["Styx_Key",       0x6450, 0x6457, 0x34, False], \
+               ["Fog_Lamp",       0x6450, 0x6457, 0x35, False], \
+               ["Shell_Flute",    0x6450, 0x6457, 0x36, False], \
+               ["Xray_Glasses",   0x6450, 0x6457, 0x37, False], \
+               ["Broken_Statue",  0x6450, 0x6457, 0x38, False], \
+               ["Glow_Lamp",      0x6450, 0x6457, 0x39, False], \
+               ["Gold_Statue",    0x6450, 0x6457, 0x3A, False], \
+               ["Love_Pendant",   0x6450, 0x6457, 0X3B, False], \
+               ["Karissa_Plant",  0x6450, 0x6457, 0x3C, False], \
+               ["Ivory_Statue",   0x6450, 0x6457, 0x3D, False], \
+               ["Bow_of_Moon",    0x6450, 0x6457, 0X3E, False], \
+               ["Bow_of_Sun",     0x6450, 0x6457, 0x3F, False], \
+               ["Bow_of_Truth",   0x6450, 0x6457, 0x40, False]
 
 
 # [name of item, hex location start, hex location end, hex number if obtained, obtained or not
-Event_list_2 = ["Fruit_of_Repun", 0x64B8, 0x64BF, 0x23, False], \
-               ["Onyx_Statue",    0x64B8, 0x64BF, 0x25, False], \
-               ["Opel_Statue",    0x64B8, 0x64BF, 0x26, False], \
+Event_list_2 = ["Onyx_Statue",    0x64B8, 0x64BF, 0x25, False], \
                ["Insect_Flute",   0x64B8, 0x64BF, 0x27, False], \
-               ["Lime_Flute",     0x64B8, 0x64BF, 0x28, False], \
-               ["Alarm_Flute",    0x64B8, 0x64BF, 0x31, False], \
+               ["Flute_of_Lime",  0x64B8, 0x64BF, 0x28, False], \
+               ["Windmill_Key",   0x64B8, 0x64BF, 0x32, False], \
                ["Prison_Key",     0x64B8, 0x64BF, 0x33, False], \
                ["Styx_Key",       0x64B8, 0x64BF, 0x34, False], \
                ["Fog_Lamp",       0x64B8, 0x64BF, 0x35, False], \
@@ -69,10 +91,44 @@ Event_list_2 = ["Fruit_of_Repun", 0x64B8, 0x64BF, 0x23, False], \
                ["Gold_Statue",    0x64B8, 0x64BF, 0x3A, False], \
                ["Love_Pendant",   0x64B8, 0x64BF, 0X3B, False], \
                ["Karissa_Plant",  0x64B8, 0x64BF, 0x3C, False], \
-               ["Ivory_Statue",   0x64B8, 0x64BF, 0x3D, False]
+               ["Ivory_Statue",   0x64B8, 0x64BF, 0x3D, False], \
+               ["Bow_of_Moon",    0x6448, 0x644F, 0X3E, False], \
+               ["Bow_of_Sun",     0x6448, 0x644F, 0x3F, False], \
+               ["Bow_of_Truth",   0x6448, 0x644F, 0x40, False]
 
 # [name of item, hex location start, hex location end, hex number if obtained, obtained or not
-armor_list = ["Psycho_Armor", 0x6434, 0x6437, 0x1C, False]
+magic_list = ["Refresh",   0x6458, 0x645F, 0x41, False], \
+             ["Paralysis", 0x6458, 0x645F, 0x42, False], \
+             ["Telepathy", 0x6458, 0x645F, 0x43, False], \
+             ["Teleport",  0x6458, 0x645F, 0x44, False], \
+             ["Recover",   0x6458, 0x645F, 0x45, False], \
+             ["Barrier",   0x6458, 0x645F, 0x46, False], \
+             ["Change",    0x6458, 0x645F, 0x47, False], \
+             ["Flight",    0x6458, 0x645F, 0x48, False]
 
-# [name of item, hex location start, hex location end, hex number if obtained, obtained or not
-shield_list = ["Sacred_Shield", 0x6438, 0x643B, 0x12, False]
+# Least significant byte is first little endian
+money_hex_loc = 0x1F02
+
+# Least significant byte is first little endian 2 bytes long
+cur_exp_hex_loc = 0x1F04
+
+# Least significant byte is first little endian 2 bytes long
+needed_exp_hex_loc = 0x1F06
+
+# how much magic points you currently have
+cur_mp_hex_loc = 0x1F08
+
+# how much magic points you have in total
+tot_mp_hex_loc = 0x1F09
+
+# how much health points you currently have
+cur_hp_hex_loc = 0x1BC1
+
+# how much health points you have in total
+tot_hp_hex_loc = 0x1BC0
+
+# how much health points you currently have
+shield_def_tot_hex_loc = 0x1C00
+
+# how much health points you have in total
+armor_tot_def_hex_loc = 0x1C01
